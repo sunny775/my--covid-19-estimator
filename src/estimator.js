3bd4d39c7a4da1453e9afc982c809c1a1d62465a
Sunny Cletus <scletus@gmail.com>
2020-04-10 16:30:39 +0100

    challenges 1-3

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,37 @@
-const covid19ImpactEstimator = (data) => data;
+import {
+    estimateCurrentInfections,
+    estimateProjectedInfections,
+    estimateSevereCases,
+    estimateBedSpaceAvialability,
+    estimateCasesForICU,
+    estimateCasesForVentilators,
+    estimateDollarsInFlight
+} from './helpers';
+
+const covid19ImpactEstimator = (data) =>{
+    let impact = 'impact'
+
+    return {
+        data:data,
+        impact:{
+           currentlyInfected: estimateCurrentInfections(data,impact),
+           infectionsByRequestedTime:estimateProjectedInfections(data,impact),
+           severeCasesByRequestedTime:estimateSevereCases(data,impact),
+           hospitalBedsByRequestedTime:estimateBedSpaceAvialability(data,impact),
+           casesForICUByRequestedTime:estimateCasesForICU(data,impact),
+           casesForVentilatorsByRequestedTime:estimateCasesForVentilators(data,impact),
+           dollarsInFlight:estimateDollarsInFlight(data,impact)
+        },
+        severeImpact:{
+           currentlyInfected: estimateCurrentInfections(data),
+           infectionsByRequestedTime:estimateProjectedInfections(data),
+           severeCasesByRequestedTime:estimateSevereCases(data),
+           hospitalBedsByRequestedTime:estimateBedSpaceAvialability(data),
+           casesForICUByRequestedTime:estimateCasesForICU(data),
+           casesForVentilatorsByRequestedTime:estimateCasesForVentilators(data),
+           dollarsInFlight:estimateDollarsInFlight(data)
+        }
+    }
+}
 
 export default covid19ImpactEstimator;
